6ddf8045f8550a029571e807955cabdaa17cf267
Francisco Escobar <91104756+fran13emCambrian@users.noreply.github.com>
2022-04-07 00:13:43 -0230

    #Upload/download

--- JobManager/Services/BlobStorageServiceAzure.cs
+++ JobManager/Services/BlobStorageServiceAzure.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using JobManager.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace JobManager.Services
 {
-    public class BlobStorageServiceAzure
+    class BlobStorageServiceAzure : IBlobStorageService
     {
-        public BlobStorageServiceAzure()
+
+        private readonly BlobServiceClient service = new BlobServiceClient(ConnectionString);
+
+        private static string ConnectionString => "DefaultEndpointsProtocol=https;AccountName=isp1004example;AccountKey=m2fGXOU8vUMOr/hCp7YGfBFcwyb1vYZKXLXO0GrsDyuMnZkOyYsSgJCUA/D5XgpTKcyAjHTxuTl5+AStkc5gVQ==;EndpointSuffix=core.windows.net";
+        private static string Container => "a00223609";
+
+        public async Task<MemoryStream> DownloadStreamAsync(string name)
         {
+            BlobClient blob = service.GetBlobContainerClient(Container).GetBlobClient(name);
+
+            if (blob.Exists())
+            {
+                var stream = new MemoryStream();
+                await blob.DownloadToAsync(stream);
+
+                stream.Position = 0;
+                return stream;
+            }
+
+            return null;
+        }
+
+        public async Task UploadStreamAsync(string name, MemoryStream stream)
+        {
+            stream.Position = 0;
+            BlobClient blob = service.GetBlobContainerClient(Container).GetBlobClient(name);
+            await blob.UploadAsync(stream);
         }
     }
 }
+
