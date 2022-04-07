228ecc0a2b3db9b1f0ee519e9917e7013e269734
Francisco Escobar <91104756+fran13emCambrian@users.noreply.github.com>
2022-04-07 00:18:18 -0230

    #BattService

---
+++ JobManager/Services/IDeviceBatteryService.cs
@@ -0,0 +1,14 @@
+using JobManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManager.Services
+{
+
+    public interface IDeviceBatteryService
+    {
+        DeviceBattery GetBattery();
+    }
+}
